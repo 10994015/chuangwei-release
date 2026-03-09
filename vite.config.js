d49aa67578fb0f451b0977a95a34878728f5463b
10994015 <a0938599191@gmail.com>
2026-03-09 17:40:25 +0800

    新增css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/components.css', 'resources/js/app.js', 'resources/js/carousel-element.js', 'resources/js/map-element.js'],
             refresh: true,
         }),
     ],
